137d77c1405ca09272e877ce575c296600e4a965
Sayantan Roy <32139604+sayantanroy-2915@users.noreply.github.com>
2023-07-31 18:46:22 +0530

    Password encryption added

--- src/main/java/com/example/rider/service/RiderService.java
+++ src/main/java/com/example/rider/service/RiderService.java
@@ -3,6 +3,7 @@ package com.example.rider.service;
 import com.example.rider.model.Rider;
 import com.example.rider.repository.RiderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,11 @@ public class RiderService {
 	@Autowired
 	private RiderRepository repo;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	public Rider addRider (Rider rider) throws Exception {
+		rider.setPassword(passwordEncoder.encode(rider.getPassword()));
 		repo.save(rider);
 		return rider;
 	}
